2f8143e2f1693cbbfd24a9c9a1ffa8f0608b073b
Guilherme Mesquita <guirozmesquita@gmail.com>
2022-05-10 09:22:19 -0400

    alteration-interfaces

--- dist/interfaces.js
+++ dist/interfaces.js
@@ -1,10 +1,30 @@
 "use strict";
+//A INTERFACE TMB TEM MODIFIERS
 const tlou = {
-    title: "The Last of Us",
-    description: "The game of Sony",
-    genre: "Action",
-    platform: ['PS3', 'PS4'],
+    title: 'The Last of Us',
+    description: "A good game!",
+    genre: "action",
+    platform: ["ps3", "ps4"],
     getSimilars: (title) => {
-        console.log(`Similar Games ${title}: Uncharted, Plage, Metro: Exodus`);
+        console.log(`Similar games to ${title}: Uncharted, A Plage Tale, Metro`);
     }
 };
+console.log(tlou.title);
+if (tlou.getSimilars) {
+    tlou.getSimilars(tlou.title);
+}
+const leftBehind = {
+    title: 'The Last Of Us = The Left Behind',
+    description: 'A extension of original game',
+    genre: 'Action',
+    originalGame: tlou,
+    newContent: ['3 hours story', 'new characters'],
+};
+//IMPLEMENTANDO UMA CLASSE COM INTERFACE:
+class creatGame {
+    constructor(t, d, g) {
+        this.title = t;
+        this.description = d;
+        this.genre = g;
+    }
+}
--- interfaces.ts
+++ interfaces.ts
@@ -1,17 +1,52 @@
-interface Game{
-    title:string;
+//A INTERFACE TMB TEM MODIFIERS
+
+interface Game{ //criação da interface
+    title: string;
     description: string;
-    genre: string;
-    platform: string[];
-    getSimilars: (title: string) => void;
+    readonly genre: string; //Só pode ser lido, mas inalterável
+    platform?: string[]; //Platform é OPCIONAL
+    getSimilars?: (title: string) => void;
 }
 
-const tlou: Game = {
-    title : "The Last of Us",
-    description: "The game of Sony",
-    genre: "Action",
-    platform: ['PS3', 'PS4'],
-    getSimilars: (title: string)=>{
-        console.log(`Similar Games ${title}: Uncharted, Plage, Metro: Exodus`)
+const tlou: Game = { //Criação do OBJETO com todos TIPOS da interface game
+    title: 'The Last of Us',
+    description: "A good game!",
+    genre: "action",
+    platform: ["ps3", "ps4"],
+    getSimilars: (title: string) => {
+        console.log(`Similar games to ${title}: Uncharted, A Plage Tale, Metro`)
+    }
+}
+
+console.log(tlou.title)
+
+if(tlou.getSimilars){
+tlou.getSimilars(tlou.title)}
+
+interface DLC extends Game{
+    originalGame: Game;
+    newContent: string[]
+}
+
+const leftBehind: DLC = {
+    title: 'The Last Of Us = The Left Behind',
+    description: 'A extension of original game',
+    genre: 'Action',
+    originalGame: tlou,
+    newContent: ['3 hours story', 'new characters'],
+}
+
+
+//IMPLEMENTANDO UMA CLASSE COM INTERFACE:
+
+class creatGame implements Game{
+    title: string;
+    description: string;
+    genre: string;
+
+    constructor(t: string, d:string, g:string){
+        this.title = t;
+        this.description = d;
+        this.genre = g;
     }
 }
